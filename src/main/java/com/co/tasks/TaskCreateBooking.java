8049eb27f9ac8549e5ceac4f9af70a50886d957f
lisandro <lisandro.betancur@hotmail.com>
2026-01-14 00:22:58 -0500

    docs: Add Javadoc documentation to TaskCreateBooking

--- src/main/java/com/co/tasks/TaskCreateBooking.java
+++ src/main/java/com/co/tasks/TaskCreateBooking.java
@@ -9,18 +9,40 @@ import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.rest.interactions.Post;
 
+/**
+ * Task for creating a new booking in the system.
+ * This task uses a Booking payload to send a POST request to the "booking" endpoint.
+ */
 public class TaskCreateBooking implements Task {
 
   private final Booking payload;
 
+  /**
+   * Constructs a TaskCreateBooking with the specified booking payload.
+   *
+   * @param payload The {@link Booking} object containing the details for the new booking.
+   */
   public TaskCreateBooking(Booking payload) {
     this.payload = payload;
   }
 
+  /**
+   * Creates an instrumented instance of TaskCreateBooking with the provided payload.
+   *
+   * @param payload The {@link Booking} object to be used as the request body.
+   * @return A {@link Performable} task ready to be executed by an {@link Actor}.
+   */
   public static Performable withPayload(Booking payload) {
     return instrumented(TaskCreateBooking.class, payload);
   }
 
+  /**
+   * Performs the task of creating a booking as the given actor.
+   * The actor attempts to send a POST request with the booking payload.
+   *
+   * @param actor The {@link Actor} performing the task.
+   * @param <T> A type that extends {@link Actor}.
+   */
   @Override
   public <T extends Actor> void performAs(T actor) {
     actor.attemptsTo(

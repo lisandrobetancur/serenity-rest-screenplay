e8554ce583d33ea0a1f9ea1f52d6addae38fe1ee
lisandro <lisandro.betancur@hotmail.com>
2026-01-15 17:38:08 -0500

    test: Add PetsTest to verify finding pets by status
    - Create `PetsTest` class using `SerenityJUnit5Extension`.
    - Implement `findPetsByStatus` to verify the retrieval of pets with "sold" status.
    - Add assertions for the HTTP status code and specific pet details (ID 690).

---
+++ src/test/java/com/co/booking/runner/PetsTest.java
@@ -0,0 +1,47 @@
+package com.co.booking.runner;
+
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+import com.co.models.pets.response.Pet;
+import com.co.questions.QuestionGetPets;
+import com.co.questions.QuestionResponseCode;
+import com.co.tasks.TaskFindPets;
+import java.util.List;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SerenityJUnit5Extension.class)
+class PetsTest {
+
+  private static final String REST_API_URL = "https://petstore.swagger.io/v2";
+
+  @Test
+  void findPetsByStatus() {
+    Actor user = Actor.named("Juanito the intern").whoCan(CallAnApi.at(REST_API_URL));
+
+    user.attemptsTo(TaskFindPets.byStatus("sold"));
+
+    user.should(seeThat("the status code", QuestionResponseCode.was(), equalTo(SC_OK)));
+
+    List<Pet> pets = new QuestionGetPets().answeredBy(user);
+
+    Pet pet =
+        pets.stream()
+            .filter(p -> p.getId() == 690)
+            .findFirst()
+            .orElseThrow(() -> new RuntimeException("Pet ID not found")); // Handle missing data
+
+    user.should(seeThat("user", actor -> pet, notNullValue()));
+
+    user.should(
+        seeThat("the category id", actor -> pet.getCategory().getId(), equalTo(6)),
+        seeThat("the name", actor -> pet.getName(), equalTo("Sharikk")),
+        seeThat("the status", actor -> pet.getStatus(), equalTo("sold")));
+  }
+}

5460ac3f953934492677f0869642c7db1c1679e2
lisandro <lisandro.betancur@hotmail.com>
2026-01-15 23:34:51 -0500

    docs: Add Javadoc to REST interaction classes
    - Add class-level documentation to `Get`, `Post`, `Put`, and `Delete` interactions.
    - Document constructors, `performAs` methods, and `to` factory methods.

--- src/main/java/com/co/interactions/Get.java
+++ src/main/java/com/co/interactions/Get.java
@@ -7,20 +7,38 @@ import net.serenitybdd.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.interactions.RestInteraction;
 
+/** A Serenity Screenplay interaction to perform a GET request. */
 public class Get extends RestInteraction {
 
   private final String resource;
 
+  /**
+   * Constructor.
+   *
+   * @param resource The resource to perform the GET request on.
+   */
   public Get(String resource) {
     this.resource = resource;
   }
 
+  /**
+   * Performs the GET request.
+   *
+   * @param actor The actor performing the action.
+   * @param <T> The type of the actor.
+   */
   @Step("{0} executes a GET on the resource #resource")
   @Override
   public <T extends Actor> void performAs(T actor) {
     rest().log().all().get(as(actor).resolve(this.resource)).then().log().all();
   }
 
+  /**
+   * Factory method to create a new instance of this interaction.
+   *
+   * @param resource The resource to perform the GET request on.
+   * @return A new instance of this interaction.
+   */
   public static Get to(String resource) {
     return instrumented(Get.class, resource);
   }
--- src/main/java/com/co/interactions/Put.java
+++ src/main/java/com/co/interactions/Put.java
@@ -7,20 +7,38 @@ import net.serenitybdd.annotations.Step;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.interactions.RestInteraction;
 
+/** A Serenity Screenplay interaction to perform a PUT request. */
 public class Put extends RestInteraction {
 
   private final String resource;
 
+  /**
+   * Constructor.
+   *
+   * @param resource The resource to perform the PUT request on.
+   */
   public Put(String resource) {
     this.resource = resource;
   }
 
+  /**
+   * Performs the PUT request.
+   *
+   * @param actor The actor performing the action.
+   * @param <T> The type of the actor.
+   */
   @Step("{0} executes a PUT on the resource #resource")
   @Override
   public <T extends Actor> void performAs(T actor) {
     rest().log().all().put(as(actor).resolve(this.resource)).then().log().all();
   }
 
+  /**
+   * Factory method to create a new instance of this interaction.
+   *
+   * @param resource The resource to perform the PUT request on.
+   * @return A new instance of this interaction.
+   */
   public static Put to(String resource) {
     return instrumented(Put.class, resource);
   }
